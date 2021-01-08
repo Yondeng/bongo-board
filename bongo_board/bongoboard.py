a90348564a93c9e874d5baaf4e744a575ff2cbbf
Deng Zong Yue <yondeng@gmail.com>
2021-01-08 18:44:37 +0800

    Version 2

--- bongo_board/bongoboard.py
+++ bongo_board/bongoboard.py
@@ -12,6 +12,8 @@ class bongo_board(gym.Env):
     }
 
     def __init__(self):
+        self.theta_step = 0.1
+        self.alpha_step = 0.1
         self.base_ball_radian = 25
         self.board_lenth = 125
         self.board_width = 4
@@ -20,7 +22,7 @@ class bongo_board(gym.Env):
         self.pendulum_pole_width = 4
         self.tau = 0.02  # seconds between state updates
         self.kinematics_integrator = 'euler'
-        self.action_space = spaces.Discrete(2)
+        self.action_space = spaces.Discrete(4)
         self.thetalimit()
         # Angle at which to fail the episode
         self.theta, self.alpha = 0., 0.
@@ -33,19 +35,25 @@ class bongo_board(gym.Env):
         err_msg = "%r (%s) invalid" % (action, type(action))
         assert self.action_space.contains(action), err_msg
         if action == 0:
-            self.theta += 0.02
-            self.alpha += 0.01
+            self.theta += self.theta_step
+            
         elif action == 1:
-            self.theta -= 0.02
-            self.alpha -= 0.01
+            self.theta -= self.theta_step
+        elif action == 2:
+            self.alpha += self.alpha_step
+        elif action == 3:
+            self.alpha -= self.alpha_step
         if self.theta > self.max_theta:
             self.theta = self.max_theta
         elif self.theta < self.min_theta:
             self.theta = self.min_theta
-
-
+        # if self.alpha >=  self.theta:
+        #     self.alpha = self.theta
+        # elif self.alpha <=  - self.theta:
+        #     self.alpha = -self.theta
         self.y, self.x = (self.base_ball_radian/2)*math.cos(self.theta),\
             (self.base_ball_radian/2)*math.sin(self.theta)
+        # print(self.alpha, self.theta)
     def thetalimit(self):
         _theta = math.atan(self.node_radian/(self.board_lenth/2))
         self.max_theta = 2.5 * (math.pi - 2 * ((math.pi/2)-_theta))
@@ -55,15 +63,18 @@ class bongo_board(gym.Env):
         screen_width = 600
         screen_height = 400
         self.fix_point = rendering.Transform(translation=(self.x, self.y))
-        self.pole_translation = rendering.Transform(rotation=math.pi/2 - self.alpha,translation=(self.x, self.y))
+        self.pole_translation = rendering.Transform(rotation=math.pi/2 ,translation=(self.x, self.y))
         if self.viewer is None:
             self.viewer = rendering.Viewer(screen_width, screen_height)
             self.orignal_point = rendering.Transform(translation=(0, 0))
             self.remapping = rendering.Transform()
+            self.pendulum_map = rendering.Transform()
             self.node = rendering.make_circle(self.node_radian)
             self.node.set_color(.5, .5, .8)
             self.node.add_attr(self.fix_point)
             self.node.add_attr(self.remapping)
+            
+            
             l, r, t, b = -self.board_lenth / 2, self.board_lenth / 2, self.board_width / 2, -self.board_width / 2
             self.board = rendering.FilledPolygon([(l+self.x, b+self.y), (l+self.x, t+self.y),
                                                  (r+self.x, t+self.y), (r+self.x, b+self.y)])
@@ -81,18 +92,22 @@ class bongo_board(gym.Env):
             self.viewer.add_geom(self.planeline)
             
             self.pendulum_pole = rendering.make_capsule(110, 4)
-            self.pendulum_map = rendering.Transform()
+            
             self.pendulum_pole.set_color(.8, .3, .3)
             self.pendulum_pole.add_attr(self.pole_translation)
+            self.pendulum_pole.add_attr(self.pendulum_map)
             self.pendulum_pole.add_attr(self.remapping)
+            
             self.viewer.add_geom(self.pendulum_pole)
         if self.state is None:
             return None
         # Edit the pole polygon vertex
+        # self.pole_translation.set_rotation()
         self.remapping.set_translation(300, 150)
         self.remapping.set_rotation(self.theta)
-        self.pendulum_map.set_translation(300 , 150)
-        self.pendulum_map.set_rotation(math.pi/2)
+        self.pendulum_map.set_translation(self.x  , self.y  )
+        self.pendulum_map.set_rotation(self.alpha )
+        
         return self.viewer.render(return_rgb_array=mode == 'rgb_array')
     
     def reset(self):
