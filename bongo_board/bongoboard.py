77a0e28a9bf50818e87577f13e8b2cffb768c206
Deng Zong Yue <yondeng@gmail.com>
2021-01-08 23:37:52 +0800

    v3.1

--- bongo_board/bongoboard.py
+++ bongo_board/bongoboard.py
@@ -27,7 +27,7 @@ class bongo_board(gym.Env):
         self.kinematics_integrator = 'euler'
         self.action_space = spaces.Discrete(4)
         self.thetalimit()
-        self.center_x, self.center_y = 
+        self.center_x, self.center_y = 300, 150
         # Angle at which to fail the episode
         self.theta, self.alpha = 0.0001, 0.
         self.viewer = None
@@ -67,52 +67,49 @@ class bongo_board(gym.Env):
         
         
         if self.viewer is None:
+            
+            self.viewer = rendering.Viewer(self.screen_width, self.screen_height)
             self.fix_point = rendering.Transform(translation=(self.x, self.y))
             self.pole_translation = rendering.Transform(translation=(0,0))
-            self.viewer = rendering.Viewer(screen_width, screen_height)
             self.orignal_point = rendering.Transform(translation=(0, 0))
             self.remapping = rendering.Transform()
             self.pendulum_map = rendering.Transform()
             self.node = rendering.make_circle(self.node_radian)
             self.node.set_color(.5, .5, .8)
             self.node.add_attr(self.fix_point)
-            self.node.add_attr(self.remapping)
             
             
             l, r, t, b = -self.board_lenth / 2, self.board_lenth / 2, self.board_width / 2, -self.board_width / 2
-            self.board = rendering.FilledPolygon([(l+self.x, b+self.y), (l+self.x, t+self.y),
-                                                 (r+self.x, t+self.y), (r+self.x, b+self.y)])
+            self.board = rendering.FilledPolygon([(l, b), (l, t),
+                                                 (r, t), (r, b)])
             self.board.add_attr(self.remapping)
             self.planeline = rendering.Line((0,142-self.node_radian),\
-                                            (screen_width,142-self.node_radian))
+                                            (self.screen_width,142-self.node_radian))
             self.planeline.set_color(0., 0., 0.)
             self.base_ball = rendering.make_circle(self.base_ball_radian/2)
-            self.base_ball.add_attr(self.orignal_point)
-            self.base_ball.add_attr(self.remapping)
+            self.base_ball.add_attr(rendering.Transform(translation = (self.center_x,self.center_y)))
             self.base_ball.set_color(0., 0., 0.8)
+            
             self.viewer.add_geom(self.base_ball)
             self.viewer.add_geom(self.board)
             self.viewer.add_geom(self.node)
             self.viewer.add_geom(self.planeline)
             
             self.pendulum_pole = rendering.make_capsule(110, 4)
-            
             self.pendulum_pole.set_color(.8, .3, .3)
-            self.pendulum_pole.add_attr(self.pole_translation)
             self.pendulum_pole.add_attr(self.pendulum_map)
-            # self.pendulum_pole.add_attr(self.remapping)
+
             
             self.viewer.add_geom(self.pendulum_pole)
         if self.state is None:
             return None
-        # Edit the pole polygon vertex
         
-        
-        # self.pole_translation.set_rotation(math.pi/2)
-        self.pendulum_map.set_translation(300 + self.x , 150 + self.y)
-        self.pendulum_map.set_rotation(self.alpha )
-        self.remapping.set_translation(300, 150)
-        self.remapping.set_rotation(self.theta)
+        self.fix_point.set_translation(self.center_x + self.x, self.center_y + self.y)
+        self.remapping.set_translation(self.center_x + self.x, self.center_y + self.y)
+        self.remapping.set_rotation(-self.theta)
+        self.pendulum_map.set_translation(self.center_x + self.x, self.center_y + self.y)
+        self.pendulum_map.set_rotation(self.alpha)
+
         
         
         return self.viewer.render(return_rgb_array=mode == 'rgb_array')
