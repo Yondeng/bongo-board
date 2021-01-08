bba27b9e53b762737902e050c22b0bd3aba7c9f6
Deng Zong Yue <yondeng@gmail.com>
2021-01-08 23:08:54 +0800

    test

--- bongo_board/bongoboard.py
+++ bongo_board/bongoboard.py
@@ -4,6 +4,7 @@ from gym import spaces, logger
 from gym.utils import seeding
 import numpy as np
 from gym.envs.classic_control import rendering
+import random
 
 class bongo_board(gym.Env):
     metadata = {
