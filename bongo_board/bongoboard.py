58c9aa6badd10fb277387ab1a285d75d07e4e33b
Deng Zong Yue <yondeng@gmail.com>
2021-01-09 18:14:04 +0800

    build bongo board model (not import kinematic algorithm )

--- bongo_board/bongoboard.py
+++ bongo_board/bongoboard.py
@@ -13,23 +13,18 @@ class bongo_board(gym.Env):
     }
 
     def __init__(self):
-        self.screen_width = 600
-        self.screen_height = 400
-        self.theta_step = 0.1
-        self.alpha_step = 0.1
-        self.base_ball_radian = 25
-        self.board_lenth = 125
-        self.board_width = 4
+        self.screen_width, self.screen_height = 600, 400
+        self.theta_step, self.alpha_step = 0.1, 0.1
+        self.base_ball_radian, self.board_lenth, self.board_width = 25, 125, 4
         self.node_radian = 4
-        self.pendulum_pole_lenth = 110
-        self.pendulum_pole_width = 4
+        self.pendulum_pole_lenth, self.pendulum_pole_width = 110, 4
         self.tau = 0.02  # seconds between state updates
         self.kinematics_integrator = 'euler'
         self.action_space = spaces.Discrete(4)
         self.thetalimit()
         self.center_x, self.center_y = 300, 150
         # Angle at which to fail the episode
-        self.theta, self.alpha = 0.0001, 0.
+        self.theta, self.alpha = 0., 0.
         self.viewer = None
         self.seed()
     def seed(self,seed=None):
@@ -46,18 +41,18 @@ class bongo_board(gym.Env):
         elif action == 2:
             self.alpha += self.alpha_step
         elif action == 3:
-            self.alpha += self.alpha_step
+            self.alpha -= self.alpha_step
         if self.theta > self.max_theta:
             self.theta = self.max_theta
         elif self.theta < self.min_theta:
             self.theta = self.min_theta
-        if self.alpha >=  math.pi/2:
-            self.alpha = math.pi/2
-        elif self.alpha <=  -math.pi/2:
-            self.alpha = -math.pi/2
+        # if self.alpha <  -self.theta:
+        #     self.alpha = self.theta
+        # elif self.alpha >  math.pi - self.theta:
+        #     self.alpha = math.pi + self.theta
         self.y, self.x = (self.base_ball_radian/2)*math.cos(self.theta),\
             (self.base_ball_radian/2)*math.sin(self.theta)
-        # print(self.alpha, self.theta)
+        
     def thetalimit(self):
         _theta = math.atan(self.node_radian/(self.board_lenth/2))
         self.max_theta = 2.5 * (math.pi - 2 * ((math.pi/2)-_theta))
@@ -67,7 +62,6 @@ class bongo_board(gym.Env):
         
         
         if self.viewer is None:
-            
             self.viewer = rendering.Viewer(self.screen_width, self.screen_height)
             self.fix_point = rendering.Transform(translation=(self.x, self.y))
             self.pole_translation = rendering.Transform(translation=(0,0))
@@ -77,6 +71,10 @@ class bongo_board(gym.Env):
             self.node = rendering.make_circle(self.node_radian)
             self.node.set_color(.5, .5, .8)
             self.node.add_attr(self.fix_point)
+            self.mass_ball = rendering.make_circle(self.base_ball_radian/2)
+            self.mass_ball.set_color(0.3,0.3,0.)
+            self.mass_ball.add_attr(rendering.Transform(translation=(self.pendulum_pole_lenth,0)))
+            self.mass_ball.add_attr(self.pendulum_map)
             
             
             l, r, t, b = -self.board_lenth / 2, self.board_lenth / 2, self.board_width / 2, -self.board_width / 2
@@ -94,6 +92,7 @@ class bongo_board(gym.Env):
             self.viewer.add_geom(self.board)
             self.viewer.add_geom(self.node)
             self.viewer.add_geom(self.planeline)
+            self.viewer.add_geom(self.mass_ball)
             
             self.pendulum_pole = rendering.make_capsule(110, 4)
             self.pendulum_pole.set_color(.8, .3, .3)
@@ -108,7 +107,7 @@ class bongo_board(gym.Env):
         self.remapping.set_translation(self.center_x + self.x, self.center_y + self.y)
         self.remapping.set_rotation(-self.theta)
         self.pendulum_map.set_translation(self.center_x + self.x, self.center_y + self.y)
-        self.pendulum_map.set_rotation(0)
+        self.pendulum_map.set_rotation(self.alpha)
 
         
         
